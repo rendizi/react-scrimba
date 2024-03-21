ec7dbbfaa3a864770babd29c9c6321cea2a881a8
Baglanov Alikhan <120701599+rendizi@users.noreply.github.com>
2024-03-21 18:47:35 +0500

    Create Navbar.js

---
+++ facts/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <img src="../icon.png" className="nav--icon"></img>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
